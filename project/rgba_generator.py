da6d837aa561e97e5a143023bb01a6ff56018d4e
grishinak <st122261@student.spbu.ru>
2024-10-04 23:08:48 +0300

    add i in range check; add exception test

--- project/rgba_generator.py
+++ project/rgba_generator.py
@@ -1,15 +1,23 @@
 from typing import Tuple, Iterator
 
-def rgba_generator() -> Iterator[Tuple[int,int,int,int]]:
+
+def rgba_generator() -> Iterator[Tuple[int, int, int, int]]:
     for r in range(256):
         for g in range(256):
             for b in range(256):
-                for a in range(0,100,2):
-                    yield(r,g,b,a)
+                for a in range(0, 100, 2):
+                    yield (r, g, b, a)
 
-def get_rgba_element(i: int) -> Tuple[int,int,int,int]:
-    gen=rgba_generator()
+
+def get_rgba_element(i: int) -> Tuple[int, int, int, int]:
+
+    total_colors = 256
+    total_alpha = 51
+    total_combinations = total_colors**3 + total_alpha
+    if i < 0 or i >= total_combinations:
+        raise IndexError("Index out of range.")
+
+    gen = rgba_generator()
     for _ in range(i):
         next(gen)
-    return(next(gen))
-
+    return next(gen)
--- tests/test_rgba_generator.py
+++ tests/test_rgba_generator.py
@@ -1,15 +1,32 @@
 import pytest
-from project.rgba_generator import (get_rgba_element)
+from project.rgba_generator import get_rgba_element
 
 
-@pytest.mark.parametrize("index, expected", [
-    (0, (0,0,0,0)),
-    (1, (0,0,0,2)),
-    (50, (0,0,1,0)),
-    (256*50, (0,1,0,0)),
-    (256*256*50, (1,0,0,0)),
-    # (256*256*256*50, (255,255,255,0)),
-    # (256*256*256*50+1, (255,255,255,2)),
-])
+@pytest.mark.parametrize(
+    "index, expected",
+    [
+        (0, (0, 0, 0, 0)),
+        (1, (0, 0, 0, 2)),
+        (50, (0, 0, 1, 0)),
+        (256 * 50, (0, 1, 0, 0)),
+        (256 * 256 * 50, (1, 0, 0, 0)),
+        # (256*256*256*50, (255,255,255,0)),    #TODO:need to optimize for big indexes
+        # (256*256*256*50+1, (255,255,255,2)),
+    ],
+)
 def test_get_rgba_element(index, expected):
+    """A test to check for an excpected result when the index is in range."""
     assert get_rgba_element(index) == expected
+
+
+@pytest.mark.parametrize(
+    "index",
+    [
+        -1,  # negative index
+        256 * 256 * 256 * 51,  # out of range
+    ],
+)
+def test_get_rgba_element_index_error(index):
+    """A test to check for an IndexError exception when the index is out of range."""
+    with pytest.raises(IndexError):
+        get_rgba_element(index)
