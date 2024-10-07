64b0ddc86fc946cee7ac8b9404603c25d8fe502c
grishinak <st122261@student.spbu.ru>
2024-10-08 00:35:04 +0300

    Add uncurry_explicit func

--- project/curry_uncarry.py
+++ project/curry_uncarry.py
@@ -4,12 +4,26 @@ from typing import Callable
 def curry_explicit(function: Callable, arity: int) -> Callable:
     if arity < 0:
         raise ValueError("Arity cannot be negative.")
-    
+
     def curried(*args):
         if len(args) > arity:
-            raise TypeError(f"Function expected at most {arity} arguments, got {len(args)}.")
+            raise TypeError(
+                f"Function expected at most {arity} arguments, got {len(args)}."
+            )
         if len(args) == arity:
             return function(*args)
         return lambda *more_args: curried(*(args + more_args))
-    
+
     return curried
+
+
+def uncurry_explicit(function: Callable, arity: int) -> Callable:
+    if arity < 0:
+        raise ValueError("Arity cannot be negative.")
+
+    def uncurried(*args):
+        if len(args) != arity:
+            raise TypeError(f"Function expected {arity} arguments, got {len(args)}.")
+        return function(*args)
+
+    return uncurried
