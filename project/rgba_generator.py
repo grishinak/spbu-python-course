18ad65b5ef33dfa2e69761c4ed9ba243329371ff
grishinak <st122261@student.spbu.ru>
2024-10-07 22:51:24 +0300

    Fix rgba A range; fix rgba tests

--- project/rgba_generator.py
+++ project/rgba_generator.py
@@ -24,7 +24,7 @@ def rgba_generator() -> Iterator[Tuple[int, int, int, int]]:
     for r in range(256):
         for g in range(256):
             for b in range(256):
-                for a in range(0, 100, 2):
+                for a in range(0, 101, 2):
                     yield (r, g, b, a)
 
 
