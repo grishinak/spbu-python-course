659f25eb263e6fd437f852d8469752eb416f1343
grishinak <st122261@student.spbu.ru>
2024-10-07 23:45:04 +0300

    Redo rgba task with generator-expression.

--- tests/test_rgba_generator.py
+++ tests/test_rgba_generator.py
@@ -11,23 +11,45 @@ from project.rgba_generator import get_rgba_element
         (51, (0, 0, 1, 0)),
         (256 * 51, (0, 1, 0, 0)),
         (256 * 256 * 51, (1, 0, 0, 0)),
-        # (256*256*256*51, (255,255,255,0)),    #TODO:need to optimize for big indexes
-        # (256*256*256*51+1, (255,255,255,2)),
+        # (256 * 256 * 256 * 50, (255, 255, 255, 100)),  # Last element (255, 255, 255, 100) #TODO: need to optimize
     ],
 )
-def test_get_rgba_element(index, expected):
-    """A test to check for an excpected result when the index is in range."""
+def test_get_rgba_element_valid(index, expected):
+    """Test valid indices to ensure correct RGBA vector is returned."""
     assert get_rgba_element(index) == expected
 
 
-@pytest.mark.parametrize(
-    "index",
-    [
-        -1,  # negative index
-        256 * 256 * 256 * 51,  # out of range
-    ],
-)
-def test_get_rgba_element_index_error(index):
-    """A test to check for an IndexError exception when the index is out of range."""
+@pytest.mark.parametrize("invalid_index", [-1, 256 * 256 * 256 * 51])
+def test_get_rgba_element_invalid(invalid_index):
+    """Test invalid indices to ensure IndexError is raised."""
     with pytest.raises(IndexError):
-        get_rgba_element(index)
+        get_rgba_element(invalid_index)
+
+
+# @pytest.mark.parametrize(
+#     "index, expected",
+#     [
+#         (0, (0, 0, 0, 0)),
+#         (1, (0, 0, 0, 2)),
+#         (50, (0, 0, 0, 100)),
+#         (51, (0, 0, 1, 0)),
+#         (256 * 51, (0, 1, 0, 0)),
+#         (256 * 256 * 51, (1, 0, 0, 0)),
+#     ],
+# )
+# def test_get_rgba_element(index, expected):
+#     """A test to check for an excpected result when the index is in range."""
+#     assert get_rgba_element(index) == expected
+
+
+# @pytest.mark.parametrize(
+#     "index",
+#     [
+#         -1,  # negative index
+#         256 * 256 * 256 * 51,  # out of range
+#     ],
+# )
+# def test_get_rgba_element_index_error(index):
+#     """A test to check for an IndexError exception when the index is out of range."""
+#     with pytest.raises(IndexError):
+#         get_rgba_element(index)
