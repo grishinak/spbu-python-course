d5013d6948d8b5373239b1218698edaeff43b20c
grishinak <st122261@student.spbu.ru>
2024-10-23 19:55:59 +0300

    Fix tests

--- tests/threadpool/test_thread_pool.py
+++ tests/threadpool/test_thread_pool.py
@@ -1,4 +1,5 @@
 import pytest
+import threading
 import time
 from project.threadpool.thread_pool import ThreadPool
 
@@ -36,13 +37,6 @@ def test_multiple_tasks(thread_pool):
     assert result == [1, 2]
 
 
-def test_pool_size():
-    pool = ThreadPool(num_threads=5)
-    time.sleep(0.1)  # give time to initialize the threads
-    assert len(pool.threads) == 5
-    pool.dispose()
-
-
 def test_dispose(thread_pool):
     def task():
         time.sleep(0.1)
@@ -66,9 +60,34 @@ def test_multiple_concurrent_tasks(thread_pool):
     assert result[0] == 5
 
 
+def test_pool_size():
+    initial_thread_count = threading.active_count()
+    pool = ThreadPool(num_threads=5)
+    time.sleep(0.1)  # give time for initialization of threads
+    assert threading.active_count() == initial_thread_count + 5
+    pool.dispose()
+
+
+def test_pool_size_2():
+    num_threads = 5
+    initial_thread_count = threading.active_count()
+    pool = ThreadPool(num_threads)
+    time.sleep(0.1)  # give time for initialization of threads
+    new_thread_count = threading.active_count()
+    assert new_thread_count - initial_thread_count == num_threads
+    pool.dispose()
+
+
 def test_pool_size_not_less_than_n():
     n = 5
     pool = ThreadPool(num_threads=n)
     time.sleep(0.1)  # give time for initialization of threads
-    assert len(pool.threads) >= n  # check that there are at least n threads in the pool
+    assert len(pool.threads) == n  # check that there are n threads in the pool
+    pool.dispose()
+
+def test_pool_size_not_less_than_n_2():
+    n = 5
+    pool = ThreadPool(num_threads=n)
+    time.sleep(0.1)  # give time for initialization of threads
+    assert not(len(pool.threads) < n)  # check that there are not less than n threads in the pool
     pool.dispose()
